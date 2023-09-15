5592f80fb84716944bfdc55f2ae2b1ecc2175402
Sujal Jain <ethicalwolf333@gmail.com>
2023-09-16 02:06:33 +0530

    Initial Commit

---
+++ client_viva/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+   return (
+      <div className='w-screen h-screen flex items-center justify-center bg-slate-900'>
+         <div className='text-center text-slate-200'>
+            <h1 className='font-bold text-4xl'>Vivacity 2k24</h1>
+            <h3 className='font-semibold text-md'>Under Development</h3>
+         </div>
+      </div>
+   );
+}
+
+export default App;
