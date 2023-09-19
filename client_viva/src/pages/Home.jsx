7c0ce3a82614e4227155392a94987a0bfd5475d2
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-09-19 10:14:05 +0530

    modified:   client_viva/index.html 	modified:   client_viva/package-lock.json 	modified:   client_viva/package.json 	modified:   client_viva/src/App.jsx 	new file:   client_viva/src/components/ArtistLineup.jsx 	new file:   client_viva/src/components/Hero.jsx 	new file:   client_viva/src/components/Navbar.jsx 	modified:   client_viva/src/index.css 	new file:   client_viva/src/pages/Home.jsx

---
+++ client_viva/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import ArtistLineup from "../components/ArtistLineup";
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+    return ( 
+    <>
+    <Navbar/>
+    <Hero/>
+    <ArtistLineup/>
+    </>
+    
+ );
+}
+ 
+export default Home;
