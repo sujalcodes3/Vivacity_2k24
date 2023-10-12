a36e600d5bc3fce8b06442910bf76bedd572c2f6
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-10-12 16:54:48 +0530

    modified:   client_viva/index.html 	deleted:    client_viva/src/App.css 	modified:   client_viva/src/components/Footer.jsx 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Black.eot 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Black.ttf 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Black.woff 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Black.woff2 	new file:   client_viva/src/fonts/MabryPro/MabryPro-BlackItalic.eot 	new file:   client_viva/src/fonts/MabryPro/MabryPro-BlackItalic.ttf 	new file:   client_viva/src/fonts/MabryPro/MabryPro-BlackItalic.woff 	new file:   client_viva/src/fonts/MabryPro/MabryPro-BlackItalic.woff2 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Bold.eot 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Bold.ttf 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Bold.woff 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Bold.woff2 	new file:   client_viva/src/fonts/MabryPro/MabryPro-BoldItalic.eot 	new file:   client_viva/src/fonts/MabryPro/MabryPro-BoldItalic.ttf 	new file:   client_viva/src/fonts/MabryPro/MabryPro-BoldItalic.woff 	new file:   client_viva/src/fonts/MabryPro/MabryPro-BoldItalic.woff2 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Italic.eot 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Italic.ttf 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Italic.woff 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Italic.woff2 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Light.eot 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Light.ttf 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Light.woff 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Light.woff2 	new file:   client_viva/src/fonts/MabryPro/MabryPro-LightItalic.eot 	new file:   client_viva/src/fonts/MabryPro/MabryPro-LightItalic.ttf 	new file:   client_viva/src/fonts/MabryPro/MabryPro-LightItalic.woff 	new file:   client_viva/src/fonts/MabryPro/MabryPro-LightItalic.woff2 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Medium.eot 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Medium.ttf 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Medium.woff 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Medium.woff2 	new file:   client_viva/src/fonts/MabryPro/MabryPro-MediumItalic.eot 	new file:   client_viva/src/fonts/MabryPro/MabryPro-MediumItalic.ttf 	new file:   client_viva/src/fonts/MabryPro/MabryPro-MediumItalic.woff 	new file:   client_viva/src/fonts/MabryPro/MabryPro-MediumItalic.woff2 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Regular.eot 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Regular.ttf 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Regular.woff 	new file:   client_viva/src/fonts/MabryPro/MabryPro-Regular.woff2 	new file:   client_viva/src/fonts/MabryPro/demo.html 	new file:   client_viva/src/fonts/MabryPro/stylesheet.css 	modified:   client_viva/src/index.css 	modified:   client_viva/tailwind.config.js 	new file:   package-lock.json

--- client_viva/src/components/Footer.jsx
+++ client_viva/src/components/Footer.jsx
@@ -1,8 +1,22 @@
+
+
 const Footer = () => {
     return ( 
     <>
-        <footer className="flex bg-slate-100">
-            
+        <footer className="flex bg-white justify-around items-center p-4">
+            <div className=" grow border-r border-gray-500">
+                <h1 className="text-xl">Contact Us:</h1>
+                <p className="text-base mt-4">Name 1 <br /> 910077868</p>
+                <p className="text-base mt-2">Name 2 <br /> 840978643</p>
+            </div>
+            <div className=" grow ">
+                Vivacity Logo
+            </div>
+            <div className=" grow border-l border-gray-500">
+                <div>
+                    Social Media Icons
+                </div>
+            </div>
         </footer>
     </> 
     );
