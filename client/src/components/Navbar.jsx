8f47d881c0dba00515b30e01a3763f21d919b59e
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2023-10-29 11:07:57 +0530

    fixed overflow in navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,18 +1,20 @@
-import { Link } from "react-router-dom";
-import Logo from "../assets/VivaLogo.png";
+import { Link } from 'react-router-dom';
+import Logo from '../assets/VivaLogo.png';
 const Navbar = () => {
-    return ( 
-        <header className="flex bg-transparent text-white text-xl justify-around items-center h-[15vh] py-4 border-b-1 border-gray-300 w-screen">
-          <nav>
-            <ul className="flex space-x-3">
-            {/* // TODO : Links are not complete */}
-              <li><Link to="/Events">Events</Link></li>
-            </ul>
-          </nav>
-          <img src={Logo} alt="Logo" className="block h-5/6"/>
-          <Link to="/CA-Portal">CA Portal</Link>
+    return (
+        <header className="flex bg-transparent text-white text-xl justify-around items-center h-[15vh] py-4 border-b-1 border-gray-300">
+            <nav>
+                <ul className="flex space-x-3">
+                    {/* // TODO : Links are not complete */}
+                    <li>
+                        <Link to="/Events">Events</Link>
+                    </li>
+                </ul>
+            </nav>
+            <img src={Logo} alt="Logo" className="block h-5/6" />
+            <Link to="/CA-Portal">CA Portal</Link>
         </header>
-     );
-}
- 
+    );
+};
+
 export default Navbar;
