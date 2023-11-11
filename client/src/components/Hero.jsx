d05f8d8346fc1afe8289a2bc64327ef41526fa21
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-11-11 09:46:27 +0530

    modified:   client/src/components/Dashboard.css 	modified:   client/src/components/Hero.jsx 	modified:   client/src/components/Navbar.jsx 	new file:   client/src/components/button.css

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import Background from '../assets/racecar.gif';
 const Hero = () => {
     return ( 
-        <div className="h-screen w-full bg-cover bg-no-repeat bg-center" style={{backgroundImage:'url('+Background+')'}}>
+        <div className="h-screen w-full bg-cover bg-no-repeat bg-[52%_0] lg:bg-center" style={{backgroundImage:'url('+Background+')'}}>
             <div className='text-white text-center flex-col justify-center items-center space-y-2 pt-[35vh]'>
                 <h1 className='text-8xl text-purple-500 font-bold'>Road to Vivacity</h1>
             </div>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,34 +1,45 @@
 import { Link } from "react-router-dom";
 import Logo from "../assets/VivaL.png";
 import { useRef } from "react";
+import "./button.css";
 const Navbar = () => {
-    const headerRef = useRef();
-    
-    const changeColor = () => {
-        if(window.scrollY > 100){
-          headerRef.current.classList.remove("bg-transparent");
-          headerRef.current.classList.add("bg-inherit");
-        }
-        else{
-          headerRef.current.classList.remove("bg-inherit");
-          headerRef.current.classList.add("bg-transparent");
-        }
-    }
-    
-    window.addEventListener('scroll',changeColor);
+  const headerRef = useRef();
 
-    return ( 
-        <header ref={headerRef} className="flex  bg-transparent text-white text-xl justify-between px-24 items-center min-h-[15vh] py-4 border-b-1 border-gray-300 w-screen fixed top-0 left-0">
-          
-          <div className=" bg-white rounded-full w-[11vh] h-[11vh] flex items-center justify-center">
-            <img src={Logo} alt="Logo" className=" h-[8vh]"/>
-          </div>
-          
-          <div className='list-none bg-blue-500 hover:bg-blue-600 text-white text-2xl py-2 px-6 rounded-lg '>
-            <Link to="/caportal">CA Portal</Link>
-          </div>
-        </header>
-    );
+  const changeColor = () => {
+    if (window.scrollY > 100) {
+      headerRef.current.classList.remove("bg-transparent");
+      headerRef.current.classList.add("bg-inherit");
+    }
+    else {
+      headerRef.current.classList.remove("bg-inherit");
+      headerRef.current.classList.add("bg-transparent");
+    }
+  }
+
+  window.addEventListener('scroll', changeColor);
+
+  return (
+    <header ref={headerRef} className="flex  bg-transparent text-white text-xl justify-between px-24 items-center min-h-[15vh] py-4 border-b-1 border-gray-300 w-screen absolute top-0 left-0">
+
+      <div className=" bg-white rounded-full flex items-center justify-center w-[11vw] h-[11vw] lg:w-[11vh] lg:h-[11vh]">
+        <img src={Logo} alt="Logo" className=" h-[8vw] lg:h-[8vh]" />
+      </div>
+      <button
+      className="flex text-white dark:text-black group relative cursor-pointer overflow-hidden whitespace-nowrap h-11 px-6  [background:var(--bg)] [border-radius:var(--radius)] transition-all shadow-[0_0_0_3px_rgba(255,255,255,0.3)_inset] hover:scale-105 duration-300  w-max  items-center justify-center  hover:shadow-[0_0_0_3px_rgba(255,255,255,0.3)_inset]">
+        <div className="absolute inset-0 overflow-visible [container-type:size]">
+          <div className="absolute inset-0 h-[100cqh] animate-slide [aspect-ratio:1] [border-radius:0] [mask:none] ">
+            <div className="absolute inset-[-100%] w-auto rotate-0 animate-spin [background:conic-gradient(from_calc(270deg-(var(--spread)*0.5)),transparent_0,hsl(0_0%_100%/1)_var(--spread),transparent_var(--spread))] [translate:0_0]">
+              </div>
+            </div>
+        </div>
+        <div className="absolute [background:var(--bg)] [border-radius:var(--radius)] [inset:var(--cut)]">
+        </div>
+        <span className="relative whitespace-pre text-center text-base font-semibold leading-none tracking-tight text-white z-10">
+          <Link to="/caportal">CA Portal</Link></span>
+      </button>
+      
+    </header>
+  );
 };
 
 export default Navbar;
