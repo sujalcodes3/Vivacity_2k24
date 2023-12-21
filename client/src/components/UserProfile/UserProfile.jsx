62d00bc3f73ec342f6913ce67cdc5ecdc795ccaa
Krishna <krishnakhattri24@gmail.com>
2023-12-21 18:17:16 +0530

    Profie page responsive

--- client/src/components/UserProfile/UserProfile.jsx
+++ client/src/components/UserProfile/UserProfile.jsx
@@ -68,102 +68,105 @@ export default function UserProfile() {
         }
         fetchUser();
     }, []);
-
+  
     return isAllowed && UserData ? (
         <div
-            className={`w-screen h-screen flex flex-col items-center justify-center select-none ${classes.entirebackground}`}
+            className={`flex flex-col items-center justify-center select-none ${classes.entirebackground}`}
         >
             <nav
-                className={`flex md:h-32 w-full justify-between items-center px-5`}
+                className={`flex md:h-32 w-full justify-between items-center px-5 mb-5 md:mb-0`}
             >
-                <img src={VivaLogo} className={`h-2/3`} />
-                <p className={`text-5xl text-violet-300 font-bold`}>
+                <img src={VivaLogo} className={`h-20 w-20 `} />
+                <p className={`text-3xl sm:text-5xl text-violet-300 font-bold`}>
                     Campus Ambassador
                 </p>
                 <button
                     onClick={logoutHandler}
-                    className={`text-red-400 text-2xl border-2 border-red-400 px-2 py-1 rounded-md hover:bg-red-400 hover:text-white transition-colors duration-150 delay-50`}
+                    className={`text-red-400 sm:text-2xl border-2 border-red-400 px-2 py-1 rounded-md hover:bg-red-400 hover:text-white transition-colors duration-150 delay-50`}
                 >
                     Logout
                 </button>
             </nav>
             <div
-                className={`w-4/5 h-4/5 border-4 flex bg-gray-800 border-gray-500 rounded-lg`}
+                className={`sm:w-4/5  h-4/5 md:flex-row flex-col border-4 flex bg-gray-800 border-gray-500 rounded-lg`}
             >
                 <section
-                    className={`w-1/3 h-full border-r-2 border-gray-500 flex items-center justify-center`}
+                    className={`md:w-1/3 md:h-full h-[300px] flex items-center justify-center`}
                 >
-                    <img src={UserProfileDummy} />
+                    <img src={UserProfileDummy} className='h-full'/>
                 </section>
-                <section className={`w-2/3 h-full p-6 flex flex-col gap-10`}>
+                <section className={`md:w-2/3 h-full border-s-2 border-gray-500  p-6 flex flex-col gap-10`}>
                     <h1
-                        className={`text-white text-5xl font-bold underline underline-offset-8`}
+                        className={`text-white sm:text-5xl text-3xl font-bold underline underline-offset-8`}
                     >
                         User Profile
                     </h1>
                     <section
-                        className={`w-full text-left items-center flex flex-col gap-6`}
+                        className={`w-full text-left items-center flex flex-col sm:gap-6 gap-y-4`}
                     >
-                        <div className={`flex justify-between w-full`}>
-                            <div>
+                        <div className={`flex justify-between sm:flex-row flex-col gap-y-4 w-full`}>
+                            <div className=''>
                                 <span
-                                    className={`font-semibold text-3xl text-gray-200`}
+                                    className={`font-semibold overflow-x-clip text-xl sm:text-3xl text-gray-200`}
                                 >
                                     Name :
-                                </span>
+                                    </span>
                                 <span
-                                    className={`font-normal text-3xl text-violet-300`}
+                                    className={`font-normal text-xl sm:text-3xl text-violet-300`}
                                 >
-                                    &nbsp;{UserData.name}
+                                    &nbsp;{UserData.name} 
                                 </span>
+                               
+
                             </div>
-                            <div>
+                            <div className=''>
                                 <span
-                                    className={`font-semibold text-3xl text-gray-200`}
+                                    className={`font-semibold text-xl sm:text-3xl text-gray-200`}
                                 >
                                     Gender :
                                 </span>
                                 <span
-                                    className={`font-normal text-3xl text-violet-300`}
+                                    className={`font-normal text-xl sm:text-3xl text-violet-300`}
                                 >
                                     &nbsp;{UserData.gender}
                                 </span>
                             </div>
                         </div>
-                        <div className={`flex justify-between w-full`}>
+                        <div className={`flex justify-between w-full sm:flex-row flex-col gap-y-4`}>
                             <div>
                                 <span
-                                    className={`font-semibold text-3xl text-gray-200`}
+                                    className={`font-semibold text-xl sm:text-3xl text-gray-200`}
                                 >
                                     Email :
                                 </span>
                                 <span
-                                    className={`font-normal text-3xl text-violet-300`}
+                                    className={`font-normal text-xl sm:text-3xl text-violet-300`}
                                 >
                                     &nbsp;{UserData.email}
                                 </span>
                             </div>
                             <div>
                                 <span
-                                    className={`font-semibold text-3xl text-gray-200`}
+                                    className={`font-semibold text-xl sm:text-3xl text-gray-200`}
                                 >
                                     Phone No. :
                                 </span>
                                 <span
-                                    className={`font-normal text-3xl text-violet-300`}
+                                    className={`font-normal text-xl sm:text-3xl text-violet-300`}
                                 >
+                                    
                                     &nbsp;{UserData.phone_number}
                                 </span>
                             </div>
                         </div>
                         <div className={`w-full`}>
                             <span
-                                className={`font-semibold text-3xl text-gray-200`}
+                                className={`font-semibold text-xl sm:text-3xl text-gray-200`}
                             >
                                 College :
                             </span>
                             <span
-                                className={`font-normal text-3xl text-violet-300`}
+                                className={`font-normal text-xl sm:text-3xl text-violet-300`}
                             >
                                 &nbsp;{UserData.college.collegename}
                             </span>
@@ -183,33 +186,33 @@ export default function UserProfile() {
                                 <ContentCopyIcon color="action" />
                             </button>
                         </div>
-                        <div className={`w-full flex gap-4`}>
+                        <div className={`w-full flex sm:flex-row flex-col gap-4`}>
                             <span
-                                className={`font-semibold text-3xl text-gray-200`}
+                                className={`font-semibold text-xl sm:text-3xl text-gray-200`}
                             >
                                 Referred Students :
                             </span>
-
-                            {hasRegCandidates ? (
+                           
+                            {  hasRegCandidates ? (
                                 <div
-                                    className={`border-2 border-slate-400 w-3/5 h-max max-h-48 rounded-md overflow-scroll bg-gray-700`}
+                                    className={`border-2 border-slate-400 sm:w-3/5 h-max max-h-40 rounded-md  overflow-y-scroll bg-gray-700`}
                                 >
                                     {UserData.referred_candidates.map(
                                         (student, ind) => (
                                             <div
                                                 key={ind}
-                                                className={`w-full h-max flex justify-between items-center border-b-2 border-slate-400`}
+                                                className={`w-full sm:h-12 h-10 overflow-clip flex justify-between items-center border-b-2 border-slate-400`}
                                             >
-                                                <span
-                                                    className={`text-3xl text-violet-300 font-normal px-2 py-1`}
+                                                <div
+                                                    className={`text-xl sm:text-3xl w-[75%] h-full overflow-x-clip text-violet-300 font-normal px-2 py-1`}
                                                 >
                                                     {student}
-                                                </span>
-                                                <span
-                                                    className={`text-3xl text-violet-500 font-normal px-4 py-1`}
+                                                </div>
+                                                <div
+                                                    className={`text-xl sm:text-3xl text-violet-500 font-normal px-4 py-1`}
                                                 >
                                                     Extra
-                                                </span>
+                                                </div>
                                             </div>
                                         ),
                                     )}
