dc5e69e08502222ec6e45fc8aec1eb959a94331c
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2023-12-22 19:48:55 +0530

    new file:   client/src/components/AfterMovie.jsx 	modified:   client/src/pages/Home.jsx

---
+++ client/src/components/AfterMovie.jsx
@@ -0,0 +1,10 @@
+const AfterMovie = () => {
+    return ( 
+        <div className="h-screen snap-start w-full">
+            <iframe className=" h-full w-full" src="https://www.youtube.com/embed/E64TKUqgi_4?si=tHHQHdYJ_8XpZ2sr" title="YouTube video player" frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+        </div>
+     );
+}
+ 
+export default AfterMovie;
