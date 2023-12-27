5306d7014bdaf1e18b8f312041f9e8f9b485066c
sujalcodes3 <ethicalwolf333@gmail.com>
2023-12-27 23:47:50 +0530

    adjusted sizing by sujal and harsh

--- client/src/components/NormalRegistration/NormNav.jsx
+++ client/src/components/NormalRegistration/NormNav.jsx
@@ -1,11 +1,11 @@
-import V24Logo from "../../assets/viva24Logo.svg";
+import V24Logo from '../../assets/viva24Logo.svg';
 
 const NormNav = () => {
-    return ( 
-        <header className=" w-full pt-16 flex justify-center items-center">
-            <img src={V24Logo} className=" h-[10.64vh] "/>
+    return (
+        <header className=" w-full pt-4 flex justify-center items-center">
+            <img src={V24Logo} className=" h-[8vh] " />
         </header>
-     );
-}
- 
+    );
+};
+
 export default NormNav;
--- client/src/components/NormalRegistration/NormalForm.jsx
+++ client/src/components/NormalRegistration/NormalForm.jsx
@@ -1,20 +1,22 @@
-import NormalButton from "../NormalButton";
-import InputBox from "./InputBox";
+import NormalButton from '../NormalButton';
+import InputBox from './InputBox';
 
 const NormalForm = () => {
-    return ( <>
-        <form className="normal-form w-5/6 mt-12 mx-auto lg:w-[47.875vw] border border-solid border-[#45C7FF]">
-            <h1 className=" mt-[55px] mb-6 text-center font-grobold text-[#57E8E8] text-3xl tracking-wider lg:text-[40px] leading-none">Pre Register Here</h1>
-            <div className="flex mb-12 justify-center flex-col gap-[35px]">
-                <InputBox text="Name"/>
-                <InputBox text="Mobile No."/>
-                <InputBox text="Email"/>
-                <InputBox text="University/Institution/Academy"/>
-                <InputBox text="Referral Code (optional)"/>
+    return (
+        <form className="normal-form  lg:w-2/5 w-[90%] rounded-md mx-auto border border-solid border-[#45C7FF]">
+            <h1 className=" my-8 text-center font-grobold text-[#57E8E8] text-2xl tracking-wider leading-none md:text-4xl">
+                Pre Register Here
+            </h1>
+            <div className="flex justify-center flex-col gap-6 md:gap-6">
+                <InputBox text="Name" />
+                <InputBox text="Mobile No." />
+                <InputBox text="Email" />
+                <InputBox text="University/Institution/Academy" />
+                <InputBox text="Referral Code (optional)" />
             </div>
-            <NormalButton text={"Next"}/>
+            <NormalButton text={'Next'} />
         </form>
-    </> );
-}
- 
+    );
+};
+
 export default NormalForm;
