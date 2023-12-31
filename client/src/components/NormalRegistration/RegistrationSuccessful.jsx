8b616512b9c06d2ff1060828fb6ae77c13049f67
sujalcodes3 <ethicalwolf333@gmail.com>
2024-01-01 01:14:44 +0530

    changed confirmation message

--- client/src/components/NormalRegistration/RegistrationSuccessful.jsx
+++ client/src/components/NormalRegistration/RegistrationSuccessful.jsx
@@ -1,8 +1,8 @@
-import Form from "./Form";
-import NormalButton from "./NormalButton";
+import Form from './Form';
+import NormalButton from './NormalButton';
 
 const RegistrationSuccessful = () => {
-    return ( 
+    return (
         <Form>
             <h1 className="w-5/6 mx-auto mt-8 mb-4 text-center font-grobold text-white text-2xl tracking-wider leading-none md:text-4xl">
                 Congratulations !
@@ -11,10 +11,13 @@ const RegistrationSuccessful = () => {
                 <br />
                 Vivacity 2024
             </h1>
-            <p className="text-white w-2/3 mx-auto md:text-xl">You will shortly receive the payment details on your registered email address.</p>
-            <NormalButton text="Home"/>
+            <p className="text-white w-2/3 mx-auto md:text-xl">
+                You will shortly be contacted by our team for payment
+                instructions.
+            </p>
+            <NormalButton text="Home" />
         </Form>
-     );
-}
- 
-export default RegistrationSuccessful;
+    );
+};
+cessful;
+export default RegistrationSuc;
