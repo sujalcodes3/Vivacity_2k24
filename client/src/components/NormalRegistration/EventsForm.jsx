6277f1512aec0f93c91b1c9315190e8a0d84ca7e
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2024-01-07 07:38:14 +0530

    modified:   client/src/components/NormalRegistration/EventsForm.jsx 	modified:   client/src/pages/NormalRegistration.jsx

--- client/src/components/NormalRegistration/EventsForm.jsx
+++ client/src/components/NormalRegistration/EventsForm.jsx
@@ -112,13 +112,6 @@ const EventsForm = ({ change, add, event, isReset, reset }) => {
                 />
             </div>
             <div className="mb-6 flex-col gap-6 hidden" ref={teamDetailsRef}>
-                <InputBox
-                    name="teamSize"
-                    value={event.teamSize}
-                    text="Team Size"
-                    change={change}
-                />
-
                 <div className=" w-5/6 mx-auto">
                     <RadioGroup
                         name="captain"
@@ -129,25 +122,7 @@ const EventsForm = ({ change, add, event, isReset, reset }) => {
                         options={['Yes', 'No']}
                     />
                 </div>
-
-                <InputBox
-                    name="teamName"
-                    value={event.teamName}
-                    text="Team Name"
-                    change={change}
-                />
-                <div className="flex flex-col gap-2">
-                    <InputBox
-                        name="teamMembers"
-                        value={event.teamMembers}
-                        text="Team Members (optional)"
-                        change={change}
-                    />
-                    <p className="text-white text-left w-5/6 mx-auto">
-                        <span className=" text-red-400">*</span> Enter member
-                        names seperated by comma
-                    </p>
-                </div>
+                
                 <div className="mx-auto w-5/6 flex justify-end pt-4 gap-4">
                     <NormalButton text="Add Event" name="add" handler={add} />
                 </div>
