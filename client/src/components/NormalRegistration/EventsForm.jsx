7ad5c7a009109521cb2ab7af09b60bc672cb6bf6
Devansh Bhatt <devanshbhatt@Devanshs-MacBook-Air.local>
2024-01-19 00:51:46 +0530

    OnSpotReg Modified

--- client/src/components/NormalRegistration/EventsForm.jsx
+++ client/src/components/NormalRegistration/EventsForm.jsx
@@ -50,7 +50,27 @@ const EventsForm = ({ change, add, event, isReset, reset }) => {
         ],
         'Fashion Events': ['Vogue'],
     };
-    const captEvents = ['Mukhauta','Rangshala','Battle of Bands','Movie Auction','Vogue','Street Dance','Razzamatazz'];
+    const captEvents = [
+        'Mukhauta',
+        'Rangshala',
+        'Battle of Bands',
+        'Movie Auction',
+        'Vogue',
+        'Street Dance',
+        'Razzamatazz',
+    ];
+
+    const spotEvents = [
+        'Hue-niverse',
+        'Contrasto',
+        'Eclectic',
+        'Tattoo Tales',
+        'Splash',
+        'Duologue',
+        'Dare to Spell',
+        'Potpourii',
+        'Family Feud',
+    ];
     const [options, setOptions] = useState(categoryOptions);
 
     const eventNameRef = useRef();
@@ -113,22 +133,33 @@ const EventsForm = ({ change, add, event, isReset, reset }) => {
                 />
             </div>
             <div className="mb-6 flex-col gap-6 hidden" ref={teamDetailsRef}>
-
-                { (captEvents.includes(event.eventName)) &&
-                <div className=" w-5/6 mx-auto">
-                    <RadioGroup
-                        name="captain"
-                        isReset={isReset}
-                        setReset={reset}
-                        change={change}
-                        heading="Are you the team captain/coordinator ?"
-                        options={['Yes', 'No']}
-                    />
-                </div>
-                }
-                <div className="mx-auto w-5/6 flex justify-end pt-4 gap-4">
-                    <NormalButton text="Add Event" name="add" handler={add} />
-                </div>
+                {captEvents.includes(event.eventName) &&
+                    !spotEvents.includes(event.eventName) && (
+                        <div className=" w-5/6 mx-auto">
+                            <RadioGroup
+                                name="captain"
+                                isReset={isReset}
+                                setReset={reset}
+                                change={change}
+                                heading="Are you the team captain/coordinator ?"
+                                options={['Yes', 'No']}
+                            />
+                        </div>
+                    )}
+                {spotEvents.includes(event.eventName) && (
+                    <div className=" w-5/6 mx-auto font-grobold text-[#57E8E8] text-xl tracking-wider leading-none md:text-2xl">
+                        Onspot Registration for this Event
+                    </div>
+                )}
+                {!spotEvents.includes(event.eventName) && (
+                    <div className="mx-auto w-5/6 flex justify-end pt-4 gap-4">
+                        <NormalButton
+                            text="Add Event"
+                            name="add"
+                            handler={add}
+                        />
+                    </div>
+                )}
             </div>
         </Form>
     );
