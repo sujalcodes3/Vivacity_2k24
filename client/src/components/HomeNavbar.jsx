f609ffeb9ebf1e51fb9c005f840774f17061fb2f
Devansh Bhatt <devanshbhatt@Devanshs-MacBook-Air.local>
2024-01-26 11:13:58 +0530

    Header and clouds fixed

--- client/src/components/HomeNavbar.jsx
+++ client/src/components/HomeNavbar.jsx
@@ -2,7 +2,7 @@ import Button from './NewHomeButton';
 import logo from '../assets/logoNew.svg';
 import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import classes from "./Footer.module.css"
+import classes from './Footer.module.css';
 // import back from "../assets/trasparentbg.svg"
 
 const HomeNavbar = () => {
@@ -13,16 +13,30 @@ const HomeNavbar = () => {
         if (location.pathname === '/newAbout') setStatus(true);
     }, []);
 
-    return (
-          <div className={`flex ${classes.header} py-4 justify-around items-center `}>
+    const aboutNavbar = (
+        <div
+            className={`flex ${classes.header} py-4 justify-around items-center `}
+        >
             <Button title="About Us" />
             <Button title="Events" />
-            {status ? <img src={logo} alt=""/> : null}
+            {status ? <img src={logo} alt="" /> : null}
             <Button title="Sponsors" />
             <Button title="Register" />
-         
         </div>
     );
+
+    const homeNavbar = (
+        <div className={`flex w-full justify-around items-center`}>
+            <Button title="About Us" />
+            <Button title="Events" />
+            <Button title="Sponsors" />
+            <Button title="Register" />
+        </div>
+    );
+
+    const navbar = status ? aboutNavbar : homeNavbar;
+
+    return navbar;
 };
 
 export default HomeNavbar;
--- client/src/components/TestNewHome.jsx
+++ client/src/components/TestNewHome.jsx
@@ -4,8 +4,8 @@ import firstLeft from '../assets/newHomePageImages/1_left.png';
 import secondLeft from '../assets/newHomePageImages/2_left.png';
 import firstRight from '../assets/newHomePageImages/3_right.png';
 import character from '../assets/newHomePageImages/HomePageCharacters.svg';
-import cloud1 from "../assets/GIFs/cloud1.gif";
-import cloud2 from "../assets/GIFs/cloud2.gif";
+import cloud1 from '../assets/GIFs/cloud1.gif';
+import cloud2 from '../assets/GIFs/cloud2.gif';
 import { useScrollTop } from '../hooks/useScroll';
 
 export default function ImageLayers() {
@@ -18,9 +18,17 @@ export default function ImageLayers() {
             <img className={`top-[5%]  absolute`} src={firstLeft} />
             <img className={`top-1/4  absolute`} src={secondLeft} />
             <img className={`top-1/4 absolute`} src={firstRight} />
-            <div className='absolute w-full top-[5%] flex justify-around '>
-                <img src={cloud2} alt="moving clouds" className=' w-1/2 relative'/>
-                <img src={cloud1} alt="moving clouds" className=' w-1/2 relative' />
+            <div className="absolute w-full top-[5%] flex justify-around ">
+                <img
+                    src={cloud2}
+                    alt="moving clouds"
+                    className=" w-1/3 relative"
+                />
+                <img
+                    src={cloud1}
+                    alt="moving clouds"
+                    className=" w-2/3 relative"
+                />
             </div>
             <img className={`top-[31.7%] absolute`} ref={ref} src={land} />
             <img
