b1db0e5a39653fe3a04681d1e3c7f3f6046b5c7c
ScrewedUP <43698882+ScrewedUP@users.noreply.github.com>
2024-01-26 06:28:42 +0000

    Character Added

--- client/src/pages/NewAboutUs.jsx
+++ client/src/pages/NewAboutUs.jsx
@@ -9,15 +9,24 @@ import aboutleft from '../assets/leftAbout.gif';
 import hello1 from '../assets/hello1.gif';
 import Star from '../components/Star';
 import HamsburgerNavbar from '../components/HamsburgerNavbar';
+import { useMediaQuery } from 'react-responsive';
 
 // import "./NewHome.css"
 
 const NewAboutUs = () => {
+    const isLargeScreen = useMediaQuery({ minWidth: 1024 });
     return (
         <div className={`${classes.aboutusbg} bg-[#FFC332] relative overflow-x-hidden`}>
             <Navbar />
+            { isLargeScreen &&
+                <div className=''>
+                <img src={aboutleft} alt='leftimg' className='absolute z-10 h-46 w-40'/>
+                <img src={hello1} alt='hello' className='absolute z-10 left-[80px] top-[140px] h-46 w-40'></img>
+            </div> 
+            }
+            
             <HamsburgerNavbar />
-            <div className="lg:mt-20 mt-10">
+            <div className="lg:mt-40 mt-10 ml-8">
                 {/* <div className=""> */}
                 <DomainTemplate data={TeamData.mentors} />
                 <DomainTemplate data={TeamData.heads} />
