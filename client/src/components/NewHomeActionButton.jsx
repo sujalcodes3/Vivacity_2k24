df0b84d10b152ef72ec278d34f830652e815a1f2
Sujal Jain <95567432+sujalcodes3@users.noreply.github.com>
2024-01-26 19:28:54 +0530

    Home Page responsive (#111) (#113)
    * new file:   client/src/assets/about-mob.svg
    	new file:   client/src/assets/events-card-mob.png
    	modified:   client/src/assets/events-card.png
    	new file:   client/src/assets/newHomePageImages/logo-mob.svg
    	new file:   client/src/assets/pronite-cardS.png
    	new file:   client/src/components/AboutUsMobile.jsx
    	modified:   client/src/components/Cards.module.css
    	modified:   client/src/components/EventCard.jsx
    	modified:   client/src/components/Footer.jsx
    	modified:   client/src/components/NewArtists.jsx
    	modified:   client/src/components/NewHomeEvents.jsx
    	modified:   client/src/components/ProniteCard.jsx
    	modified:   client/src/components/Pronites.jsx
    	modified:   client/src/pages/newHomePage.jsx
    
    * modified:   client/src/components/AboutUsMobile.jsx
    	modified:   client/src/components/EventCard.jsx
    	modified:   client/src/components/Pronites.jsx
    	modified:   client/src/pages/newHomePage.jsx
    
    * HOme PAge CHanges
    
    * home page changes
    
    ---------
    
    Co-authored-by: Saket Samarth <84617740+Saket2766@users.noreply.github.com>

--- client/src/components/NewHomeActionButton.jsx
+++ client/src/components/NewHomeActionButton.jsx
@@ -3,7 +3,7 @@ import classes from './actionButton.module.css';
 const ActionButton = ({ title }) => {
     return (
         <div
-            className={`${classes.actionbutton} cursor-pointer bg-no-repeat w-[18.5rem] h-[4.3125rem] flex font-grobold p-2 items-center justify-center text-white text-2xl hover:text-navButtonYellow z-20 `}
+            className={`${classes.actionbutton} cursor-pointer bg-no-repeat lg:w-[18.5rem] w-[14rem] py-3 lg:h-[4.3125rem] w-2/3 h-max flex font-grobold lg:p-2 items-center justify-center text-white lg:text-2xl text-lg hover:text-navButtonYellow z-20 `}
         >
             <p className="drop-shadow-[0_0_4.8px_#5C2E00]">{title}</p>
         </div>
