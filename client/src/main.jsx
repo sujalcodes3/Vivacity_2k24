815370ade947205bf06eca049bb93cd5ed28601d
nayangoyal <101893656+nayangoyal@users.noreply.github.com>
2024-01-29 16:04:08 +0000

    Added "Supersonic Rocketship" Font and shadow card

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import './fonts/supersonic_rocketship1/Supersonic-Rocketship.ttf'
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
