6d273733741845be6d033ab34d31e7ca7ca3684b
sujalcodes3 <ethicalwolf333@gmail.com>
2024-01-30 00:28:49 +0530

    fixed:register button was not working on the events page on the divs which had a character on the right side.

--- client/src/components/Events/RegisterComponent.jsx
+++ client/src/components/Events/RegisterComponent.jsx
@@ -4,7 +4,7 @@ import Register from '../../assets/EventsImages/Register.png';
 
 const RegisterComponent = ({rulebook}) => {
     return (
-        <div className="flex flex-row px-[20%] gap-2 lg:px-0">
+        <div className="flex flex-row px-[20%] gap-2 lg:px-0 z-[100]">
             {rulebook?.length > 0 && <a href={rulebook} target='_blank' className={`cursor-pointer z-[100]`} >
                 <img className="lg:w-2/3 min-w-[120px]" src={RuleBook} alt="" />
             </a>}
