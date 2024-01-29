4b7d039abf2b2a1769d6f40b84f0701c691f40b0
Saket2766 <84617740+Saket2766@users.noreply.github.com>
2024-01-30 01:57:45 +0530

    modified:   client/src/components/NormalRegistration/NormNav.jsx

--- client/src/components/NormalRegistration/NormNav.jsx
+++ client/src/components/NormalRegistration/NormNav.jsx
@@ -1,9 +1,11 @@
+import { useNavigate } from 'react-router-dom';
 import V24Logo from '../../assets/viva24Logo.svg';
 
 const NormNav = () => {
+    const nav = useNavigate();
     return (
         <header className=" w-full py-12 lg:py-4 flex justify-center items-center">
-            <img src={V24Logo} className=" h-[12vh] " />
+            <img src={V24Logo} className=" h-[12vh] " onClick={ (e)=>{nav('/')}} />
         </header>
     );
 };
