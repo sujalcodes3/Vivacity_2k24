46e1ec5bf3baa5e271200bf44587d436e28ea261
nayangoyal <101893656+nayangoyal@users.noreply.github.com>
2024-02-01 06:27:56 +0000

    Solved "ScrollToTop" Issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,10 +15,13 @@ import EventsPage from './pages/EventsPage';
 
 import NewAboutUs from './pages/NewAboutUs';
 
+import ScrollToTop from './components/ScrollToTop';
+
 function App() {
 
     return (
         <BrowserRouter>
+        <ScrollToTop />
              <Routes>
                 <Route path="/" element={<NewHomePage />} />
                 <Route path='/about' element={<NewAboutUs/>}/>
